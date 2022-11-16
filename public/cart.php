96a337add18a3f12aa2a2be284c973e390957c86
Twan <61585746+TwanOttink@users.noreply.github.com>
2022-11-16 12:00:46 +0100

    Basis design

--- public/cart.php
+++ public/cart.php
@@ -1,17 +1,7 @@
 <?php
 include "header.php";
-include "cartFunctions.php"
-?>
-<div class="row">
-    <div class="col-12">
-        <h1>Winkelmandje</h1>
-    </div>
-    <div class="col-12 border-bottom"></div>
-    <div class="col-12">
-        
-    </div>
-</div>
-<?php
+include "cartFunctions.php";
+
 $total = 0;
 // kijken of er op de plusknop is gedrukt.
 if (isset($_GET['addId'])) {
@@ -25,39 +15,60 @@ if (isset($_GET['removeId'])) {
 if (isset($_GET['fullRemoveId'])) {
     removeItem($_GET['fullRemoveId'], true);
 }
-
-foreach (getCart() as $key => $item) {
-    //Haal item op
-    $stockItem = getStockItem($key, $databaseConnection);
-    print("<div class='col-12 bg-light mb-1 rounded shadow'>");
-        if ($stockItemImage = getStockItemImage($key, $databaseConnection)) {
-            // gaat naar
-            print("<img height='100px' width='auto' src='img/stock-item/" . $stockItemImage[0]['ImagePath'] . "' alt=''></img>");
-        } else {
-            print("<img height='100px' width='auto' src='img/stock-group/" . $stockItem['BackupImagePath'] . "' alt=''></img>");
-        }
-//        echo $stockItem['StockItemName'] . " " . round($stockItem['SellPrice'], 2);
-
-        print ("<h4>" . $stockItem['StockItemName'] . "<br>" . "€" . round($stockItem['SellPrice'], 2) . "</h4>");
-
-        $total += round($stockItem['SellPrice'], 2) * $item;
-        print("<a href='cart.php?addId=" . $key . "'>+</a>");
-        print($item);
-        print("<a href='cart.php?removeId=" . $key . "'>-</a>");
-        print("<a href='cart.php?fullRemoveId=" . $key . "' class='text-danger'> Verwijder</a>");
-    print("</div>");
-}
-
-print("Totaal: €$total");
 ?>
-<div class="col-6">
-    <a href="https://www.ideal.nl/demo/qr/?app=ideal">Bestelling plaatsen</a>
+<div class="row">
+    <div class="col-12">
+        <h1>Winkelmandje</h1>
+        <hr>
+    </div>
+    <div class="card border-0">
+        <div class="row">
+            <?php
+            foreach (getCart() as $key => $item) {
+                print("<div class='col-8'><div class='row'><div class='row align-items-center'>");
+                //Haal item op
+                $stockItem = getStockItem($key, $databaseConnection);
+
+                // als key niet bestaat, ga door.
+                if (!$stockItem) {
+                    continue;
+                }
+
+                if ($stockItemImage = getStockItemImage($key, $databaseConnection)) {
+                    print("<div class='col-2'><img class='img-fluid' src='img/stock-item/" . $stockItemImage[0]['ImagePath'] . "'></div>");
+                } else {
+                    print("<div class='col-2'><img class='img-fluid' src='img/stock-group/" . $stockItem['BackupImagePath'] . "'></div>");
+                }
+
+                print ("<div class='col'><div class='row'>" . $stockItem['StockItemName'] . "</div></div>");
+                print ("<div class='col'><a href='cart.php?addId=" . $key . "'>+</a> " . $item . "<a href='cart.php?removeId=" . $key . "'> -</a></div>");
+                print ("<div class='col'>&euro;" . round($stockItem['SellPrice'], 2) . "<span class='close'><a href='cart.php?fullRemoveId=" . $key . "' class='text-danger'>&#10005;</a></span></div>");
+                print("</div></div></div>");
+                $total += round($stockItem['SellPrice'], 2) * $item;
+            }
+            ?>
+            <div class="col-4">
+                <h5>
+                    <b>Overzicht</b>
+                </h5>
+                <hr>
+                <div class="row">
+                    <div class="col">Totaal</div>
+                    <div class="col text-right">&euro; <?php print(str_replace('.', ',', $total)) ?></div>
+                </div>
+                <hr>
+                <div class="text-right">
+                    <a href="https://www.ideal.nl/demo/qr/?app=ideal" class="btn btn-primary">Bestelling plaatsen</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <?php
 include "footer.php";
 ?>
 
 <script>
-    // Verwijder URL GET query om dubbele uitvoering te voorkomen op refresh.
-    window.history.pushState("object or string", "Title", "/"+window.location.href.substring(window.location.href.lastIndexOf('/') + 1).split("?")[0]);
+    // Verwijder URL GET-query om dubbele uitvoering te voorkomen op ververs.
+    window.history.pushState("object or string", "Title", "/" + window.location.href.substring(window.location.href.lastIndexOf('/') + 1).split("?")[0]);
 </script>
--- public/cartFunctions.php
+++ public/cartFunctions.php
@@ -8,14 +8,14 @@ function getCart()
     if (isset($_SESSION['cart'])) {
         $cart = $_SESSION['cart'];
     } else {
-        $cart = array();                            //zo nee: dan een nieuwe (nog lege) array
+        $cart = array();                                //zo nee: dan een nieuwe (nog lege) array
     }
-    return $cart;                               // resulterend winkelmandje terug naar aanroeper functie
+    return $cart;                                       // resulterend winkelmandje terug naar aanroeper functie
 }
 
 function saveCart($cart)
 {
-    $_SESSION["cart"] = $cart;                  // werk de "gedeelde" $_SESSION["cart"] bij met de meegestuurde gegevens
+    $_SESSION["cart"] = $cart;                          // werk de "gedeelde" $_SESSION["cart"] bij met de meegestuurde gegevens
 }
 
 function addItem($stockItemID)
@@ -24,7 +24,7 @@ function addItem($stockItemID)
     if (array_key_exists($stockItemID, $cart)) {        //controleren of $stockItemID(=key!) al in array staat
         $cart[$stockItemID] += 1;                       //zo ja: aantal met 1 verhogen
     } else {
-        $cart[$stockItemID] = 1;                    //zo nee: key toevoegen en aantal op 1 zetten.
+        $cart[$stockItemID] = 1;                        //zo nee: key toevoegen en aantal op 1 zetten.
     }
     saveCart($cart);
 }
@@ -32,13 +32,13 @@ function addItem($stockItemID)
 function removeItem($stockItemID, $deleteAll = false)
 {
     $cart = getCart();
-    if(!array_key_exists($stockItemID, $cart)) {    // Item niet in winkelmand verwidjeren? return niks
+    if(!array_key_exists($stockItemID, $cart)) {        // Item niet in winkelmand verwidjeren? return niks
         return;
     }
-    if ($cart[$stockItemID] <= 1 || $deleteAll) {   // Kijkt of er nog 1 over is of als deleteAll is meegebracht
-        unset($cart[$stockItemID]);                 // Verwijder item uit sessie
+    if ($cart[$stockItemID] <= 1 || $deleteAll) {       // Kijkt of er nog 1 over is of als deleteAll is meegebracht
+        unset($cart[$stockItemID]);                     // Verwijder item uit sessie
     } else {
-        $cart[$stockItemID]--;                      //  Aantal items -1
+        $cart[$stockItemID]--;                          //  Aantal items -1
     }
     saveCart($cart);
 }
