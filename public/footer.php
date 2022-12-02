84f44f17c38feccf9864cf8e337c69b7839156de
Rafael Muijsert <rmuijsert@gmail.com>
2022-12-02 10:35:28 +0100

    fix cache miss for demo

--- public/footer.php
+++ public/footer.php
@@ -40,8 +40,8 @@
 <!--        </div>-->
 <!--    </footer>-->
 <!--</div>-->
-<!--<script>-->
-<!--    if ( window.history.replaceState ) {-->
-<!--        window.history.replaceState( null, null, window.location.href );-->
-<!--    }-->
-<!--</script>-->
+<script>
+    if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+    }
+</script>
