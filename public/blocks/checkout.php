dd39b7c60c4d9e72e61662df7f24da58adef8730
Maart <maartenhemme50@gmail.com>
2022-12-05 22:23:15 +0100

    Styled order.php and checkout.php

--- public/blocks/checkout.php
+++ public/blocks/checkout.php
@@ -1,8 +1,80 @@
-<section class="summary">
+<?php
+    $total = 0;
+    if(count($_POST) > 0) {
+        $_SESSION['userinfo'] = $_POST;
+    }
+    $databaseConnection = $GLOBALS['databaseConnection'];
+?>
+<section class="checkout">
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                checkout
+            <div class="col-4">
+                <div class="checkout__wrapper bg-white">
+                    <h5>Ingevulde gegevens</h5>
+
+                    <?php
+                        $firstname = $_SESSION['userinfo']['firstname'];
+                        $prefixName = $_SESSION['userinfo']['prefixName'];
+                        $surname = $_SESSION['userinfo']['surname'];
+
+                        $postalzip = $_SESSION['userinfo']['postcode'];
+                        $city = $_SESSION['userinfo']['city'];
+                        $adress = $_SESSION['userinfo']['street'];
+                    ?>
+
+                    <ul>
+                        <li><?= $firstname . " $prefixName" . " $surname" ?></li>
+                        <li><?= $_SESSION['userinfo']['email'] ?></li>
+                        <?php if (isset($_SESSION['userinfo']['phone'])): ?>
+                            <li><?= $_SESSION['userinfo']['phone'] ?></li>
+                        <?php endif; ?>
+                    </ul>
+
+                    <ul>
+                        <li><?= $postalzip . ", " . $city  ?></li>
+                        <li><?= $adress  ?></li>
+                    </ul>
+
+                    <span>Uitzonderingen</span>
+                    <?php if (isset($_SESSION['userinfo']['comment'])): ?>
+                        <p><?= $_SESSION['userinfo']['comment'] ?></p>
+                    <?php else: ?>
+                        <p>---</p>
+                    <?php endif; ?>
+
+                    <h5>Bezorgmoment</h5>
+                    <p>Dinsdag 25 November - 10:30</p>
+                </div>
+            </div>
+            <div class="col-8">
+                <div class="checkout__wrapper bg-white bg-white--large">
+                    <h2>Te bestellen producten</h2>
+
+                    <div class="div">
+                        // Alle producten
+                    </div>
+
+                    <a href="?action=pay" class="btn btn--order">Ga naar betalen</a>
+                    <?php
+                    if(isset($_GET['action']) && $_GET['action'] == 'pay') {
+                        addKlant($_SESSION['userinfo']['fname'], $_SESSION['userinfo']['lname'], $_SESSION['userinfo']['email'], $_SESSION['userinfo']['phone'], $databaseConnection);
+                        $klantID = findKlant($databaseConnection);
+                        addOrder($klantID[0]['max(klantID)'], $_SESSION['userinfo']['country'], $_SESSION['userinfo']['street'], $_SESSION['userinfo']['postcode'], $_SESSION['userinfo']['city'], $databaseConnection);
+                        $orderID = findOrder($databaseConnection);
+                        foreach ($_SESSION['cart'] as $id => $quantity) {
+                            $total = 0;
+                            $stockItem = getStockItem($id, $GLOBALS['databaseConnection']);
+                            $price = round($stockItem['SellPrice'], 2);
+                            $total += $price * $quantity;
+                            addOrderregel($orderID[0]['max(OrderID)'], $id, $quantity, $total, $databaseConnection);
+                            removeStock($id, $quantity, $databaseConnection);
+                        }
+                        ?>
+                        <script>
+                            window.location.replace('https://www.ideal.nl/demo/qr/?app=ideal');
+                        </script>
+                    <?php } ?>
+                </div>
             </div>
         </div>
     </div>
--- public/blocks/order.php
+++ public/blocks/order.php
@@ -3,70 +3,120 @@
         <div class="row">
             <div class="col-8">
                 <div class="order__wrapper bg-white bg-white--large">
-                    <h2 class=text-center>Gegevens invullen</h2><hr>
+                    <h2>Gegevens invullen</h2>
+                    <p>Vul in de onderstaande Velden uw in:</p>
                     <form method="post" action="checkout.php" class="order__form">
 
-                        <div class="order__form-row">
+                        <div class="order__form-row order__form-row--40">
+                            <?php
+                            $firstname = '';
+                            if (isset($_SESSION['userinfo']['firstname'])):
+                                $firstname = $_SESSION['userinfo']['firstname'];
+                            endif; ?>
+                            <label for="firstname">Voornaam:*</label>
+                            <input class="input" placeholder="Voornaam" value="<?= $firstname ?>" type="text" id="firstname" name="firstname" required>
+                        </div>
+
+                        <div class="order__form-row order__form-row--20">
+                            <?php
+                            $prefixName = '';
+                            if (isset($_SESSION['userinfo']['prefixName'])):
+                                $prefixName = $_SESSION['userinfo']['prefixName'];
+                            endif; ?>
+                            <label for="prefixName">Tussenvoegsel:</label>
+                            <input class="input" placeholder="Tussenvoegsel" value="<?= $prefixName ?>" type="text" id="prefixName" name="prefixName">
+                        </div>
+
+                        <div class="order__form-row order__form-row--40">
+                            <?php
+                            $surname = '';
+                            if (isset($_SESSION['userinfo']['surname'])):
+                                $surname = $_SESSION['userinfo']['surname'];
+                            endif; ?>
+                            <label for="surname">Achternaam:*</label>
+                            <input class="input" placeholder="Achternaam" value="<?= $surname ?>" type="text" id="surname" name="surname" required>
+                        </div>
+
+                        <div class="order__form-row order__form-row--50">
+                            <?php
+                            $birthDate = '';
+                            if (isset($_SESSION['userinfo']['birthDate'])):
+                                $birthDate = $_SESSION['userinfo']['birthDate'];
+                            endif; ?>
+                            <label for="birthDate">Geboortedatum:*</label>
+                            <input class="input" placeholder="Geboortedatum" value="<?= $birthDate ?>" type="date" id="birthDate" name="birthDate" required>
+                        </div>
+
+                        <div class="order__form-row order__form-row--50">
                             <?php
                             $mail = '';
                             if (isset($_SESSION['userinfo']['email'])):
                                 $mail = $_SESSION['userinfo']['email'];
                             endif; ?>
-                            <label for="email">Email:</label>
-                            <input placeholder="Vul hier in..." value="<?= $mail ?>" type="email" id="email" name="email"
-
-                                   style="width: 80%;" required><br><br>
+                            <label for="email">Email:*</label>
+                            <input class="input" placeholder="Emailadres" value="<?= $mail ?>" type="email" id="email" name="email" required>
                         </div>
 
-                        <div class="order__form-row">
+                        <div class="order__form-row order__form-row--50">
                             <?php
                             $phone = '';
                             if (isset($_SESSION['userinfo']['phone'])):
                                 $phone = $_SESSION['userinfo']['phone'];
                             endif; ?>
                             <label for="number">Telefoonnummer:</label>
-                            <input placeholder="Vul hier in..." value="<?= $phone ?>" type="text" id="phone" name="phone" style="width: 80%;"><br><br>
+                            <input class="input" placeholder="Telefoonnummer" value="<?= $phone ?>" type="tel" id="phone" name="phone">
                         </div>
 
-                        <div class="order__form-row">
-                            <select name="country" id="country" style="width: 80%;">
+                        <div class="order__form-row order__form-row--50">
+                            <label for="country">Land:</label>
+                            <select class="select country" name="country" id="country">
                                 <option value="Netherlands">Nederland</option>
                             </select>
                         </div>
 
-                        <div class="order__form-row">
+                        <div class="order__form-row order__form-row--50">
                             <?php
                             $city = '';
                             if (isset($_SESSION['userinfo']['city'])):
                                 $city = $_SESSION['userinfo']['city'];
                             endif; ?>
-                            <label for="city">Stad:</label>
-                            <input placeholder="Vul hier in..." value="<?= $city ?>" type="text" id="city" name="city" style="width: 80%" required><br><br>
+                            <label for="city">Stad:*</label>
+                            <input class="input" placeholder="Stad" value="<?= $city ?>" type="text" id="city" name="city" required>
                         </div>
 
-                        <div class="order__form-row">
+                        <div class="order__form-row order__form-row--50">
                             <?php
                             $street = '';
                             if (isset($_SESSION['userinfo']['street'])):
                                 $street = $_SESSION['userinfo']['street'];
                             endif; ?>
-                            <label for="street">Adress:</label>
-                            <input type="text" id="street" name="street" placeholder="Vul hier in..." value="<?= $street ?>" style="width: 80%;" required><br><br>
+                            <label for="street">Straat + huisnummer:*</label>
+                            <input class="input" type="text" id="street" name="street" placeholder="Vul hier in..." value="<?= $street ?>" required>
                         </div>
 
-                        <div class="order__form-row">
+                        <div class="order__form-row order__form-row--50">
                             <?php
                             $postalZip = '';
                             if (isset($_SESSION['userinfo']['postcode'])):
                                 $postalZip = $_SESSION['userinfo']['postcode'];;
                             endif; ?>
-                            <label for="postcode">Postcode:</label>
-                            <input placeholder="Vul hier in..." value="<?= $postalZip ?>" type="text" id="postcode" name="postcode" style="width: 80%;" required><br><br>
+                            <label for="postcode">Postcode:*</label>
+                            <input class="input" placeholder="Vul hier in..." value="<?= $postalZip ?>" type="text" id="postcode" name="postcode" required>
+                        </div>
+
+                        <div class="order__form-row">
+                            <?php
+                            $comment = '';
+                            if (isset($_SESSION['userinfo']['comment'])):
+                                $comment = $_SESSION['userinfo']['comment'];;
+                            endif; ?>
+                            <label for="comment">Opmerkingen:</label>
+                            <textarea class="input" placeholder="Vul hier in..." value="<?= $comment ?>" type="text" id="comment" name="comment"></textarea>
                         </div>
 
                         <div class="order__form-row">
                             <input TYPE="hidden" NAME="required_fields" VALUE="name, from">
-                            <input class="shadow-lg w-100 btn btn-primary" type="submit" value="Bevestig gegevens">
+                            <input class="btn btn--order" type="submit" value="Bevestig gegevens">
                         </div>
                     </form>
                 </div>
