64a502301263dbde053f164ddab624b3ccc2adcb
Maart <maartenhemme50@gmail.com>
2022-12-20 23:16:01 +0100

    Suggestion for new footer

--- public/footer.php
+++ public/footer.php
@@ -5,31 +5,35 @@
                 <div class="footer__inner">
                     <div class="footer__nav">
                         <span>Pagina's</span>
-                        <menu>
+                        <ul>
                             <li><a href="/">T-shirts</a></li>
                             <li><a href="/">Chocolade</a></li>
                             <li><a href="/">Novelties</a></li>
                             <li><a href="/">Kleding</a></li>
                             <li><a href="/">USB Novelties</a></li>
                             <li><a href="/">Alle categoriën</a></li>
-                        </menu>
-                    </div>
-                    <div class="footer__cookies">
-                        <menu style="float: bottom; position: relative;">
-                        <a href="cookies.php">Cookies </a>  |
-                        <a href="privacy.php"> Privacy policy </a>  |
-                        <a href="tos.php"> Algemene Voorwaarden </a>  |
-                        <a href="newsletter.php"> Nieuwsbrief </a>  |
-                        <a href="contact.php"> Contact</a>
-                        </menu>
+                        </ul>
                     </div>
                     <div class="footer__brand" style="float: top; height: 200px; position: relative;">
                         <a href="./" class="header__logo" id="LogoA">
                             NG
                         </a>
                     </div>
-
+                </div>
+                <div class="footer__bottom">
+                    <div class="footer__cookies">
+                        <ul style="float: bottom; position: relative;">
+                            <a href="cookies.php">Cookies </a>
+                            <a href="privacy.php"> Privacy policy </a>
+                            <a href="tos.php"> Algemene Voorwaarden </a>
+                            <a href="newsletter.php"> Nieuwsbrief </a>
+                            <a href="contact.php"> Contact</a>
+                        </ul>
+                        <div><a href="/">©Huttenhuis Installatietechniek 2021</a></div>
+                    </div>
+                </div>
             </div>
+
         </div>
     </div>
 </footer>
