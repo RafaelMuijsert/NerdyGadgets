7f40f1604972598cb578c0b632318b68fdecb902
Maart <maartenhemme50@gmail.com>
2022-12-21 15:25:14 +0100

    Add 404 page to directory

---
+++ public/404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>Pagina niet gevonden - NerdyGadgets</title>
+
+    <!-- Javascript -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/popper.min.js"></script>
+
+    <!-- Style sheets-->
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+</head>
+<body>
+
+<?php
+    include "header.php";
+    include './blocks/404.php';
+    include "footer.php";
+?>
+
+<script>
+    if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+    }
+</script>
+</body>
+</html>
+
