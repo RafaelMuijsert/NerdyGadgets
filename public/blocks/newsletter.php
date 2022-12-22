fbc8f163a3731bad29a1a73ae852928cd7134dc2
reed <[jessevaandering@gmail.com]>
2022-12-22 19:39:54 +0100

    newsletter checkbox on edit-data-form.php

--- public/blocks/newsletter.php
+++ public/blocks/newsletter.php
@@ -6,34 +6,18 @@
                     <div>
                         <h2>Nieuwsbrief</h2>
                     </div>
-
-
-                    <span class="order__form-row order__form-error">
-                        <?php
-                        if(isset($_POST) && isset($_POST['submit-email-Registration'])):
-
-                            $_SESSION['email-registration'] = $_POST;
-                            $username = $_SESSION['email-registration']['email'];
-
-                            maillistaccount(
-                                $_SESSION['email-registration']['email'],
-                                $_SESSION['email-registration']['firstname'],
-                                $_SESSION['email-registration']['prefixName'],
-                                $_SESSION['email-registration']['surname'],
-                                $databaseConnection,
-                            );
-                        endif;
-                        ?>
-                    </span>
                     <br>
-                    <h5 style="font-weight: 500">Blijf up-to-date met onze Nieuwsbrief!</h5>
+                    <h5 style="font-weight: 500">Registreer nu en blijf up-to-date met onze Nieuwsbrief!</h5>
                     <ul class="newsletter__pros">
-                        <li>U krijgt als enige kortingscodes!</li>
-                        <li>U weet als een van de eerste de opkomende aanbiedingen!</li>
-                        <li>U weet als eerste van nieuwe producten!</li>
+                        <li>U krijgt exclusief toegang tot kortingscodes!</li>
+                        <li>U hoort het als een van de eerste van opkomende aanbiedingen!</li>
+                        <li>U weet als eerste van nieuwe producten af!</li>
                     </ul>
                     <br>
-                    <?php include "form/newsletter-form.php" ?>
+                    <div class="form__form-row">
+                        <a href="./register.php" class="btn btn--order" type="submit">REGISTREER NU!</a>
+                    </div>
+                    <br>
                 </div>
             </div>
         </div>
